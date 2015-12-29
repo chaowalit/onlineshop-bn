c4231797ce78e9741da88ad86fa5f2fbe08a7b4d
chaowalit <dekchaowalit_123@hotmail.com>
2015-12-30 01:43:46 +0700

    update

--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
     ],
 
     /*
